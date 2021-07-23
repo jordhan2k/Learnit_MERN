618a63ceb1228c29490275d136e72024197dfc63
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-07-23 08:44:51 +0700

    update server file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const authRouter = require('./routes/authentication');
 const postRouter = require('./routes/post');
 
@@ -25,7 +26,7 @@ connectDB();
 const app = express();
 // enable server to read json objects
 app.use(express.json());
-
+app.use(cors());
 
 
 // Authentication routes
