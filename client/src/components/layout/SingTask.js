da05c360696878f636d799c6e8919d2234978d8b
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-08-09 09:07:09 +0700

    Update dashboard

--- client/src/components/layout/SingTask.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SingleTask = ({title}) => {
-    return (
-        <div className="single-task-div">title</div>
-    )
-}
-
-export default SingleTask;
--- client/src/components/views/Dashboard.js
+++ client/src/components/views/Dashboard.js
@@ -1,49 +1,101 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react'
 import { AuthContext } from '../../context/AuthContext';
-
-
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { PostContext } from '../../context/PostContext';
+import Spinner from '../../utils/Spinner';
+import SinglePost from '../posts/SinglePost';
+import PostCreateModal from '../posts/PostCreateModal';
+
 
 
 const Dashboard = () => {
     const { logoutUser, authState } = useContext(AuthContext);
+    const { getPosts,
+        postState: {
+            posts,
+            postLoading },
+        showCreatePostModal,
+        setShowCreatePostModal,
+        showToast: {
+            show,
+            message,
+            type
+        },
+        setShowToast
+
+
+    } = useContext(PostContext);
+
+
+    // START: get all posts
+    useEffect(() => getPosts(), []);
+
+    useEffect(() => {
+        if (show) {
+            setTimeout(()=> setShowToast({
+                show: false,
+                message: '',
+                type: null
+            }), 3000);
+        }
+    }, [show]);
+
+
+    let body = null;
+    if (postLoading) {
+        body = (<Spinner />);
+    } else {
+        if (posts.length === 0) {
+            body = (<SinglePost post={{ 'title': 'No post found' }} />);
+        } else {
+            body = posts.map(item => (
+
+                <SinglePost key={item._id} post={item} />
+
+
+            ));
+
+
+
+
+
+
+
+
+        }
+    }
+
+
 
     return (
-        <div className="app-container">
-            <div className="navbar">
-                <div className="user-area">
-                    Hello, {authState.user.username}
-                </div>
-                <div onClick={() => {
-                    if (window.confirm("Do you want to logout?")) {
-                        logoutUser();
-                    }
-                }}> <ExitToAppIcon /></div>
-            </div>
-
-
-            <div className="task-container">
-                <div className="single-task-div">sdfsfds</div>
-
-
+        <>
+            <PostCreateModal />
+            {show && <div className={`warning post-toast ${type}`}>{message}</div>}
+            <div className="app-container">
+                {/* <div className="background"></div> */}
+                <div className="navbar">
+                    <div className="user-area">
+                        Hello, {authState.user.username}!
+                    </div>
+                    <div onClick={() => {
+                        if (window.confirm("Do you want to logout?")) {
+                            logoutUser();
+                        }
+                    }}> <ExitToAppIcon /></div>
                 </div>
 
 
+                <div className="task-container">
+                    {body}
+                </div>
 
-
-
-
-
-
-
-
-                <div className="add-button">
+                <div title="Let's create a task" className="add-button" onClick={() => setShowCreatePostModal(!showCreatePostModal)}>
                     <div className="vertical-stroke"></div>
                     <div className="horizontal-stroke"></div>
                 </div>
             </div>
-            );
+        </>
+    );
 }
 
-            export default Dashboard;
+export default Dashboard;
