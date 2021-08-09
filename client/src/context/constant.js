3326676c98ee811854f00c03c107ebf286dc78af
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-08-09 09:08:47 +0700

    Add constants

--- client/src/context/constant.js
+++ client/src/context/constant.js
@@ -1,3 +1,8 @@
 export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'smth';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
+
+export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS';
+export const POSTS_LOADED_FAIL = 'POSTS_LOADED_FAIL';
+export const POST_CREATED = 'POST_CREATED';
+export const POST_NOT_CREATED = 'POST_NOT_CREATED';
