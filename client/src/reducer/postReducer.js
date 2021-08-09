2904e5d2cb86f4ec8b95e8dd70ff9182e25d8e1f
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-08-09 09:09:19 +0700

    Update post reducer

--- client/src/reducer/postReducer.js
+++ client/src/reducer/postReducer.js
@@ -1,14 +1,28 @@
+import { POSTS_LOADED_FAIL, POSTS_LOADED_SUCCESS, POST_CREATED } from "../context/constant";
+
 export const postReducer = (state, action) => {
     const { type, payload } = action;
 
     switch (type) {
-        case 'POSTS_LOADED_SUCCESSFULLY':
+        case POSTS_LOADED_SUCCESS:
             return {
                 ...state,
                 posts: payload,
                 postLoading: false
             };
-    
+        case POSTS_LOADED_FAIL:
+            return {
+                ...state,
+                posts: payload,
+                postLoading: false
+            };
+
+        case POST_CREATED:
+            return {
+                ...state,
+                posts: [...state.posts, payload]
+               
+            }
         default:
             return state;
     }
