fd15a45182de3984874474b5470ea0b3fa191cd8
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-08-10 01:39:15 +0700

    Add link to deployed backend

--- client/src/context/constant.js
+++ client/src/context/constant.js
@@ -1,5 +1,5 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'smth';
-
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://pacific-lowlands-52624.herokuapp.com/api' : 'https://pacific-lowlands-52624.herokuapp.com/api';
+// http://localhost:5000/api
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
 
 export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS';
